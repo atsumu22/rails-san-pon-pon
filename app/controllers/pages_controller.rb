eeeaaf91e4c96b088ff54981cf79297e9a3eb1fc
Mariama KaiKai <mmak.webdev@gmail.com>
2023-02-11 16:04:58 +0900

    routes done for core user journey for the chairperson

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :dashboard ]
 
-  def home
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#dashboard"
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :shops, only: %i[new create]
+
+  #stamp cards are nested inside of the participating shops because we need to identify the specific shop in order to give the correct shop its stamp ---> one stamp belongs to one shop
+  resources :shop_participants, only: %i[index] do
+    resources :stamp_cards, only: %i[new create]
+  end
+
+  resources :stamp_rallies, only: %i[index show new create]
+  resources :shops, only: %i[index show new create]
 end
