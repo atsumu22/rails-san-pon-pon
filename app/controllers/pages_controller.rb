439ff1d8754b0c89ced2fc9badc844a93f00bef9
Maria Cosmen <mcosme00@gmail.com>
2023-02-22 23:03:50 +0900

    Edit view for stamp cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,6 @@ class PagesController < ApplicationController
   def dashboard
     @shops = policy_scope(Shop).last(4)
     @stamprallies = policy_scope(StampRally).all
+    @participant = current_user
   end
 end
