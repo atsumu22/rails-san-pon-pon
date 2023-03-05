8152a12fb2494c3ec529b37c8a8903d99e5159b9
Maria Cosmen <mcosme00@gmail.com>
2023-03-05 12:07:59 +0900

    comment AJAX changes

--- app/javascript/controllers/rallies_tab_controller.js
+++ app/javascript/controllers/rallies_tab_controller.js
@@ -10,15 +10,15 @@ export default class extends Controller {
   // }
 
   hideAllRallies() {
-    console.log(this.ralliesTargets[0].children.children)
+    console.log(this.ralliesTargets[0].children)
   }
 
   // to get the clicked tab
   switchTab(event) {
     event.preventDefault()
-
+    // this.hideAllRallies()
     const target = event.target.getAttribute("data-rallies-tab-target")
-    console.log(target);
+    // console.log(target);
     let filter = ""
     if (target === "comingSoonRallies") {
       filter = "coming_soon=1"
@@ -26,18 +26,54 @@ export default class extends Controller {
       filter = "ongoing=1"
     }
     const url = `/stamp_rallies?${filter}`
-    // window.location.href = url
+    window.location.href = url
 
-    fetch(url, { headers: { "X-Requested-With": "XMLHttpRequest" } })
-      .then(response => console.log(response.text()))
 
-      .then(html => {
-        console.log(target);
-        let container = this.ralliesTargets[0];
-        // .getAttribute(`[data-rallies-tab-target="${target}"]`)
-        console.log(container);
-        container.innerHTML = target
-      })
+
+    // my experiment to try to make AJAX WORK...
+
+    // TO GET EACH RALLY CARD
+
+    // all the children:
+    // let children = this.ralliesTargets[0].children
+    // console.log(children);
+    // // this returns an object and I need to iterate through it
+
+    // // to check if this element has a class with ongoin
+    // console.log(this.ralliesTargets[0].children[0].children[0].className.includes("soon"))
+    // console.log(this.ralliesTargets[0].children[0].children[0].className)
+
+    // let condition = ""
+    // if (target === "ongoingRallies") {
+    //   condition = "ongoing"
+    // } else if (target === "comingSoonRallies") {
+    //   condition = "soon"
+    // }
+
+
+    // const filtered = children.map(children => children[0].children[0].className.includes(condition) )
+    // console.log(filtered);
+
+
+
+    // fetch(url, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+    //   .then(response => console.log(response.text()))
+
+    //   .then(html => {
+    //     // console.log(target);
+    //     // let container = this.ralliesTargets[0].children.children;
+    //     let condition = ""
+    //     const filtered = children.map(children => children[0].children[0].className.includes(condition) )
+    //     console.log(filtered);
+    //     if (target === "ongoingRallies") {
+    //       condition = "ongoing"
+    //     } else if (target === "comingSoonRallies") {
+    //       condition = "soon"
+    //     }
+
+    //     // .getAttribute(`[data-rallies-tab-target="${target}"]`)
+    //     console.log(condition);
+    //   })
   }
 
   showAll() {
